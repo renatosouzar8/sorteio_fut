f7b1b39d4eeae1168b257b5ba8fb4e81a64df074
renatosouzar8 <renator8@yahoo.com.br>
2026-05-02 00:58:20 -0300

    Hardcode DB URL for connection test

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,6 +6,12 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient()
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: "postgresql://postgres.jowpixphwzvpkcscrntl:SorteioFut2026@aws-1-sa-east-1.pooler.supabase.com:6543/postgres?pgbouncer=true"
+      }
+    }
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
